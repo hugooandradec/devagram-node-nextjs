3b4d571f303ab89f2ed1772d35f1568b14b7ce28
Victor Hugo <hugooandradec@gmail.com>
2023-03-24 21:35:47 -0300

    add feed

--- pages/api/usuario.ts
+++ pages/api/usuario.ts
@@ -1,8 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import type { RespostaPadraoMsg } from "@/types/RespostaPadraoMsg";
 import { validarTokenJWT } from "@/middlewares/validarTokenJWT";
+import { conectarMongoDB } from "@/middlewares/conectarMongoDB";
+import { UsuarioModel } from "@/models/UsuarioModel";
 
-const usuarioEndpoit = (req: NextApiRequest, res: NextApiResponse) => {
-    return res.status(200).json('Usuario autenticado com sucesso');
+const usuarioEndpoit = async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg | any>) => {
+    try{
+        const {userId} = req?.query;
+        const usuario =await UsuarioModel.findById(userId);
+        usuario.senha = null;
+        return res.status(200).json(usuario);
+    }catch(e){
+        console.log(e);
+    }
+
+    return res.status(400).json({erro: 'Não foi possível obter dados do usuário'})    
 }
 
-export default validarTokenJWT(usuarioEndpoit);
+export default validarTokenJWT(conectarMongoDB(usuarioEndpoit));
